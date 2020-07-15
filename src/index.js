48abf7c116fed4d709c83d86c3841c9a9a761fe9
yuriyspz <55743745+yuriyspz@users.noreply.github.com>
2020-07-15 22:08:50 +0300

    redux structure

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import reducers from './reducers'
+import {Provider} from 'react-redux';
+import thunk from "redux-thunk";
 
+const store = createStore(reducers, applyMiddleware(thunk));
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <App/>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
